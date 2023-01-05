bd4779eee5d44b423c49b63e0c24a234c68a6387
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2023-01-05 10:45:08 +0100

    init

---
+++ modules/partner-stream/main.tf
@@ -0,0 +1,20 @@
+resource "aws_sqs_queue" "aggregate_queue" {
+  name = var.partner_name
+}
+
+resource "aws_lambda_function" "aggregate_lambda" {
+
+  filename      = "java-basic.jar"
+  function_name = "aggregate"
+  role          = aws_iam_role.aggregate_lambda.arn
+  handler       = "example.HandlerInteger"
+
+  runtime = "java11"
+}
+
+resource "aws_lambda_event_source_mapping" "aggregate_lambda" {
+  event_source_arn  = aws_sqs_queue.aggregate_queue.arn
+  enabled           = true
+  function_name     = aws_lambda_function.aggregate_lambda.arn
+  batch_size       = 1
+}
